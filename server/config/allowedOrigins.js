9b5289ee4165fc6dfed6f2c51f179d4805bdc967
Ira Strubhar <taylor.strubhar@gmail.com>
2023-02-21 10:43:47 -0800

    spelling error....

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-    'http://locolhost:3000',
+    'http://localhost:3000',
     'https://www.irishrhubarb.com',
-    'https://irishrhubarb.com'
+    'https://irishrhubarb.com',
 ]
 
 module.exports = allowedOrigins
