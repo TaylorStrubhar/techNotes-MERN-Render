16ed91d6250d1200d23613701a7717deb42f34a8
Ira Strubhar <taylor.strubhar@gmail.com>
2023-03-08 14:51:13 -0800

    server ready to be deployed

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://www.irishrhubarb.com',
-    'https://irishrhubarb.com',
+    'https://technotes-sgre.onrender.com'
 ]
 
 module.exports = allowedOrigins
